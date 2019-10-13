81d22f5102f797d577a7391f64cf4c2955cd3bd0
Abel Fernandez <abelfergodevelop@gmail.com>
2019-10-13 16:51:18 +0200

    Add html report for cucumber

--- src/test/java/com/abeldevelop/blog/blogcategoryapi/cucumber/CucumberTest.java
+++ src/test/java/com/abeldevelop/blog/blogcategoryapi/cucumber/CucumberTest.java
@@ -7,15 +7,11 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-//		features = {
-//				"classpath:features/category/create-category.feature",
-//				"classpath:features/category/update-category.feature",
-//				"classpath:features/category/find-category-by-code.feature"
-//		},
 		features = "classpath:features", 
 		plugin = {
 				"progress", 
-				"json:target/cucumber-report.json"
+				"json:target/cucumber-report.json",
+				"de.monochromata.cucumber.report.PrettyReports:target/pretty-cucumber"
 		}
 )
 public class CucumberTest {
